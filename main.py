661a20a5855b208f29e05492523039a885c7e0ac
soprettypink <soprettypink@gmail.com>
2025-03-29 10:20:09 +0900

    Imported other modules

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #main.py
+import animals
+import zoo
+import file_io
 
 #Creates a Zoo instance
+
 
 #Uses a 'try-except statement to either 
 #Add animals manually or Load from file
 
 #Provides interface to list, add, save,
-#load, or remove V
+#load, or remove

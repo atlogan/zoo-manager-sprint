f64a3cba66d3616fad83a10936bbd21a58add479
soprettypink <soprettypink@gmail.com>
2025-03-29 09:09:41 +0900

    No chnages have been made

---
+++ main.py
@@ -0,0 +1,9 @@
+#main.py
+
+#Creates a Zoo instance
+
+#Uses a 'try-except statement to either 
+#Add animals manually or Load from file
+
+#Provides interface to list, add, save,
+#load, or remove V

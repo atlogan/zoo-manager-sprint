003803f114fc1784d166048342a0f6d0f1e1ea77
soprettypink <soprettypink@gmail.com>
2025-03-31 18:19:32 +0900

    Updated the Zoo instance and added Animals class

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import file_io
 
 #Creates a Zoo instance
 class Zoo:
-    def __init__(self, species):
+    def __init__(species):
         self.species = species
 
 #Uses a 'try-except statement to either 
 #Add animals manually or Load from file
+class Animals:
+    def animal_name(self, mame):
+        '''Animal name will be printed using this method'''
+        self.name = name
+
+    def make_sound(self, sound):
+        '''Animal makes a sound using this method'''
+        self.sound = sound
 
 #Provides interface to list, add, save,
 #load, or remove
